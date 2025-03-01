b8951cc2a2ee087f980e4d736070696c7eba322e
Hứa Thị Thanh Hương <thanhhuong@MacBook-Air-cua-Hua.local>
2025-03-01 23:52:03 +0700

    BFS

--- python/Graph.py
+++ python/Graph.py
@@ -1,3 +1,4 @@
+from collections import deque
 class Graph:
     def __init__(self):
         self.adj_list = {}
@@ -14,26 +15,50 @@ class Graph:
 class DepthFirstSearch:
     def __init__(self, adj_list, start_vertex):
         self.adj_list = adj_list
-        self.visited = {}
+        self.visited = [False] * len(adj_list)
         self.dfs(start_vertex)
 
     def dfs(self, vertex):
         self.visited[vertex] = True
-        print(vertex, end=" ")
-
+        print(vertex)
         for neighbor in self.adj_list[vertex]:
-            if neighbor not in self.visited:
-                print("visited: ", self.visited)
+            if self.visited[neighbor] == False:
+                print(self.visited)
                 self.dfs(neighbor)
 
+
+class BreadthFirstSearch:
+    def __init__(self, adj_list, start_vertex):
+        self.adj_list = adj_list
+        self.visited = [False] * len(adj_list)
+        self.bfs(start_vertex)
+
+    def bfs(self, start_vertex):
+        print("BFS")
+        queue = deque()
+        queue.append(start_vertex)
+        self.visited[start_vertex] = True
+
+        while queue:
+            vertex = queue.popleft()
+            print(vertex)
+
+            for neighbor in self.adj_list[vertex]:
+                if not self.visited[neighbor]:
+                    queue.append(neighbor)
+                    self.visited[neighbor] = True
+
+
+        
 
 graph = Graph()
 
 for i in range(0, 7):
     graph.add_vertex(i)
 
-graph.add_edge(0, 1)
+
 graph.add_edge(0, 6)
+graph.add_edge(0, 1)
 graph.add_edge(0, 2)
 graph.add_edge(0, 5)
 
@@ -42,6 +67,9 @@ graph.add_edge(4, 3)
 graph.add_edge(3, 5)
 
 dfs = DepthFirstSearch(graph.adj_list, 0)
+
+
+bfs = BreadthFirstSearch(graph.adj_list, 0)
 
 
 
