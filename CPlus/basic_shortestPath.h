9b98cf36e797418d19fccbe5b14d2c2e1b5f1c55
thanh thieu <vanthanh333999@gmail.com>
2025-03-09 12:44:52 +0700

    adj List shortest

--- CPlus/basic_shortestPath.h
+++ CPlus/basic_shortestPath.h
@@ -3,6 +3,9 @@
 #include <queue>
 #include <vector>
 #include <algorithm>
+#include <list>
+#include <unordered_map>
+#include <unordered_set>
 //Approach:
 // 1 use queue to store the nodes
 // 2 use a vector to store the distance of each node from the source
@@ -21,33 +24,79 @@ namespace G1 {
     class UnWeightGraph {
         public:
             int m_vertex;
-            vector< vector<int> > adj;
+            std::unordered_map<int, std::list<int>> adjList;
             UnWeightGraph(int v) {
                 this->m_vertex = v;
                 for (int i = 0; i < v; i++) {
-                    adj.emplace_back();
+                    addVertex(i);
                 }
             }
             void addEdge(int u, int v) {
-                adj[u].push_back(v);
-                adj[v].push_back(u);
+                adjList[u].push_back(v);
+                adjList[v].push_back(u);
             }
-            void addVetex() {
-                m_vertex++;
-                adj.emplace_back();
+            void addVertex(int v) {
+                if (adjList.find(v) == adjList.end())
+                    adjList[v] = std::list<int>();
             }
+          
     };
 
-    void findShortestPath(vector< vector<int> > adj, int start, int end) {
+    std::vector<int> constructPath(int endVertex, std::unordered_map<int, int>& predecessors) {
+        std::vector<int> path;
+        for (int vertex = endVertex; vertex != -1; vertex = predecessors[vertex]) {
+            path.push_back(vertex);
+        }
+        std::reverse(path.begin(), path.end()); // Reverse to get the correct order
+        return path;
+    }
+
+    vector<int>  findShortestPath( std::unordered_map<int, std::list<int>> adjList, int start, int end) {
         //implement BFS to tralverse
         //keep track of vertex's predecessor
         std::queue<int> queue;
+        std::unordered_map<int, int> predecessors;
+        std::unordered_set<int> visited;
+
+        queue.push(start);
+        visited.insert(start);
+        predecessors[start] = -1;
+        while ( !queue.empty()) {
+            int current = queue.front();
+            queue.pop();
+            if (current == end) {
+                return constructPath(end, predecessors);
+                //return result
+            }
+            for (int next : adjList[current]) {
+                //Check visited
+                //if not visited, add to queue and mark as visited
+                //keep track of the predecessor
+                if (visited.find(next) == visited.end()) {
+                    visited.insert(next);
+                    queue.push(next);
+                    predecessors[next] = current;
+                }
+            }
+        }
+        return {};
+
     }
 
     
 
     void run() {
         cout << "Hello from G1" << endl;
+        UnWeightGraph g(5);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 3);
+        g.addEdge(2, 3);
+        g.addEdge(3, 4);
+        vector<int> v = findShortestPath(g.adjList, 0, 4);
+        for (int i : v) {
+            cout << i << " ";
+        }
 
     }
 }
