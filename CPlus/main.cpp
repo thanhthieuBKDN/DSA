a1782707c38d0173f63c4bbc4e5fb6b8cf38d13a
Hứa Thị Thanh Hương <thanhhuong@MacBook-Air-cua-Hua.local>
2025-03-15 23:18:02 +0700

    Dijkstra

--- CPlus/main.cpp
+++ CPlus/main.cpp
@@ -4,12 +4,75 @@
 #include <algorithm>
 #include <queue>
 #include "optimalShortest.h"
-#include "Dijkstra_problem.h"
+#include <climits>
 using namespace std;
 
+class Node {
+    public:
+        int vertex;
+        int weight;
+        Node(int v, int w) : vertex(v), weight(w) {
+            std::cout << "vertex " << vertex << " weight " << weight << '\n';
+        }
+        bool operator > (const Node &otherNode) const {
+            return weight > otherNode.weight;
+        }
+};
+
+void printSolution(const std::vector<int>& distances) {
+    std::cout << "Vertex\t| Distance from Source" <<
+    std::endl;
+    for (int i = 0; i < distances.size(); i++) {
+        std::cout << i << "\t|\t" 
+            << (distances[i] == INT_MAX ? "INF" : std::to_string(distances[i])) <<
+        std::endl;
+    }
+}
+
+void Dijkstra(const vector< vector<int> > &graph, int source) {
+    
+    vector <bool> visited(graph.size(), false);
+    vector <int> distances(graph.size(), INT_MAX);
+    priority_queue< Node, vector<Node>, greater<Node> > queue;
+
+    distances[source] = 0;
+    queue.emplace(Node(source, 0));
+    while( !queue.empty() ) {
+        Node current = queue.top();
+        queue.pop();
+
+        int current_vertex = current.vertex;
+        if(visited[current_vertex]) 
+        {
+            continue;
+        }
+        visited[current_vertex] = true;
+        for ( int i = 0; i < graph.size(); i++ ) {
+            if(graph[current_vertex][i] > 0 && !visited[i]) {
+                int newDist = distances[current_vertex] + graph[current_vertex][i]; //he quy chieu
+                if (newDist < distances[i]) {
+                    distances[i] = newDist;
+                    queue.emplace(Node(i, distances[i]));
+                }
+            }
+        }
+
+    }
+    printSolution(distances);
+}
 int main() {
     
-    G3::run();
-    
+   int source = 0;
+    std::vector<std::vector<int>> graph = {
+    {0, 6, 0, 1, 0},
+    {6, 0, 5, 2, 2},
+    {0, 5, 0, 0, 5},
+    {1, 2, 0, 0, 1},
+    {0, 2, 5, 1, 0}
+    };
+    Dijkstra(graph, source);
+
+
+
     return 0;
 }
